64d3ea91285309a19f0dd2ebeda663fa7586430f
johndinh391 <johndinh391@cs104u-2c.acad.fullerton.edu>
2019-09-07 13:30:26 -0700

    Inches and feet program made.

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,20 @@
 // This program calculates a person's height in feet (') and inches (").
+#include <iostream>
+#include <string>
+
+int main()
+{
+  int inches_given;
+
+  std::cout << "How tall are you in inches?"<< std::endl;// Ask how tall a person is in inches.
+
+  std::cin >> inches_given;
+
+  int inches_to_feet = inches_given / 12; // Find how many feet they are by dividing the inches given by twelve.
+
+  int inches_leftover = inches_given % 12; // Find how many inches are left over.
+
+  std::cout << "You are "<< inches_to_feet << "'" << inches_leftover << "\"." << std::endl; // Output the results.
+
+  return 0;
+}
